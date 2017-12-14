9f0115741fd65c269458706e5f864ca2728f9439
lijun <lijun@xkeshi.com>
2017-12-14 14:43:57 +0800

    OK

--- src/main/java/com/junli/Application.java
+++ src/main/java/com/junli/Application.java
@@ -14,5 +14,6 @@ public class Application {
         System.out.println("SpringBoot开始启动。。。。");
         SpringApplication.run(Application.class, args);
         System.out.println("SpringBoot启动成功");
+        System.out.println("主分支合并到子分支");
     }
 }
