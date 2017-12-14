bdacc14d68f5b532ca72a85285359988472088e7
lijun <lijun@xkeshi.com>
2017-12-14 14:35:58 +0800

    修改启动类

--- src/main/java/com/junli/Application.java
+++ src/main/java/com/junli/Application.java
@@ -11,6 +11,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        System.out.println("SpringBoot开始启动。。。。");
         SpringApplication.run(Application.class, args);
+        System.out.println("SpringBoot启动成功");
     }
 }
